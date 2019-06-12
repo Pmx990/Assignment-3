6dca1c72ac1245ee6e64ada00707524ebc78e078
Junzhe Liang <junzhe.liang@bellevuecollege.edu>
2019-06-11 22:54:29 -0700

    Create Open.java

---
+++ src/NotePad/Open.java
@@ -0,0 +1,61 @@
+import javax.swing.*;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+
+public class Open {
+    JTextPane d;
+    Recent r;
+    Open(JTextPane dx,UiStructure ui,Recent r) {
+        d=dx;
+        JFileChooser jc = new JFileChooser();
+        jc.setFileSelectionMode(JFileChooser.FILES_ONLY);
+        jc.showDialog(new JLabel(),"Select");
+        File file = jc.getSelectedFile();
+        d.setText(file.toString());
+        try{
+            int temp=0;
+            InputStream in = new FileInputStream(file);
+            StringBuffer sb = new StringBuffer();
+            while(temp!=-1){
+                temp = in.read();
+                char by = (char)temp;
+                sb.append(by);
+            }
+            System.out.println(sb.toString());
+            d.setText(sb.toString());
+            in.close();
+            r.setRecent(file.getAbsolutePath(),ui);
+
+        }catch (java.io.FileNotFoundException ff){
+            System.out.println("File not found");
+        }catch (java.io.IOException io){
+
+        }
+    }
+    Open(String adr,JTextPane dx){
+        d=dx;
+        File file = new File(adr);
+
+       // d.setText();
+        try{
+            int temp=0;
+            InputStream in = new FileInputStream(file);
+            StringBuffer sb = new StringBuffer();
+            while(temp!=-1){
+                temp = in.read();
+                char by = (char)temp;
+                sb.append(by);
+            }
+            System.out.println(sb.toString());
+            d.setText(sb.toString());
+            in.close();
+
+        }catch (java.io.FileNotFoundException ff){
+            System.out.println("File not found");
+        }catch (java.io.IOException io){
+
+        }
+    }
+
+}
