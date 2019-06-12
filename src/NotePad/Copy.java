d02fba858f930525f48c4e0cabfe70460802afa7
Junzhe Liang <junzhe.liang@bellevuecollege.edu>
2019-06-11 22:54:36 -0700

    Create Copy.java

---
+++ src/NotePad/Copy.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+/*
+* Though copy is simple, but i still think it need a single class for future update like "copy picture"
+* */
+
+public class Copy {
+    Copy(JTextPane d){
+    d.copy();
+    }
+}
